1358405335a9f1640f69c6bd98c5f9dc28e52a40
benleejc <benleejc@gmail.com>
2025-07-31 00:36:15 +0800

    Fix mkdir bug in db_config

--- src/spot_futures_arbitrage/db/db_config.py
+++ src/spot_futures_arbitrage/db/db_config.py
@@ -3,5 +3,5 @@ import os
 
 BASE_DIR = Path.cwd()
 if not os.path.exists(BASE_DIR / 'data'):
-    os.makedir (BASE_DIR / 'data')
+    os.mkdir(BASE_DIR / 'data')
 DB_PATH = BASE_DIR / 'data' / 'okx_prices.db'
